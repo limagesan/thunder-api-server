328f9dff3130117800418d8fe2138e7e077bc342
limagesan <limage31@gmail.com>
2017-12-05 10:59:57 +0900

    modify(main.go): get port env

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	router := httprouter.New()
 	router.GET("/", Logging(Index, "index"))
 	router.GET("/todos", Logging(TodoIndex, "todo-index"))
 	router.GET("/todos/:todoId", Logging(TodoShow, "todo-show"))
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
